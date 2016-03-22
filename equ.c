9807e198e509d22d905cf1a6a17e0d7d7bc1804a
DashProk <dashkos-kokos@mail.ru>
2016-03-22 10:57:44 +0600

    Change spaces on tab

--- equ.c
+++ equ.c
@@ -3,23 +3,23 @@
 
 int equ(int a, int b, int c)
 {
-    int d, x1, x2;
-    d = b*b - 4*a*c;
+	int d, x1, x2;
+	d = b * b - 4 * a * c;
 
-    if (d < 0)
-    {
-	printf("\nNo roots");
-    }
-    else if (d == 0)
-    {
-	x1 = -b/(2*a);
-	printf("\nRoot: %d", x1);
-    }
-    else if (d > 0)
-    {
-	x1 = (-b + sqrt(d))/(2*a);
-	x2 = (-b - sqrt(d))/(2*a);
-	printf("\nRoots: %d %d", x1, x2);
-    }
-    return 0;
+	if (d < 0)
+	{
+		printf("\nNo roots");
+	}
+	else if (d == 0)
+	{
+		x1 = -b / (2 * a);
+		printf("\nRoot: %d", x1);
+	}
+	else if (d > 0)
+	{
+		x1 = (-b + sqrt(d)) / (2 * a);
+		x2 = (-b - sqrt(d)) / (2 * a);
+		printf("\nRoots: %d %d", x1, x2);
+	}
+	return 0;
 }
