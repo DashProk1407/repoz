7e82d4e8fe49853eb9a0e971b07fcf16755d5fa1
DashProk <dashkos-kokos@mail.ru>
2016-04-12 22:20:31 +0600

    Decorating)

--- src/equ.c
+++ src/equ.c
@@ -12,23 +12,24 @@ int equ(double a, double b, double c, double *x1, double *x2)
 	{
 		return INV_ARG;
 	}
-	
+
 	if (a == 0)
 	{
 		*x1 = -c / b;
 		return ONEROOT;
 	}
-	
+
 	if (d < 0)
 	{
 		return NOROOTS;
 	}
-	
+
 	if (d == 0)
 	{
 		*x1 = -b / (2 * a);
 		return ONEROOT;
 	}
+
 	*x1 = (-b + sqrt(d)) / (2 * a);
 	*x2 = (-b - sqrt(d)) / (2 * a);
 	return TWOROOTS;
